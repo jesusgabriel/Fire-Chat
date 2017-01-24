3b2e40128c2eae594cc5b17576f72b04d38c11f9
jesusgabriel <amelo@fvi.edu>
2017-01-24 14:53:27 -0500

    set sass and log facebook

--- js/main.js
+++ js/main.js
@@ -1,47 +1,51 @@
-
-
-  var config = {
+var config = {
     apiKey: "AIzaSyBugCs6DhuUHK0DaFqikDs4Av8elMSuQ0Q",
     authDomain: "fire-chat-35eea.firebaseapp.com",
     databaseURL: "https://fire-chat-35eea.firebaseio.com",
     storageBucket: "fire-chat-35eea.appspot.com",
     messagingSenderId: "351839237659"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
-var db= firebase.database();
+var db = firebase.database();
 var messegesRef = db.ref("messeges/");
-var provider = new firebase.auth.FacebookAuthProvider();
-firebase.auth().signInWithPopup(provider).then(function(result) {
-  var token = result.credential.accessToken;
-  var user = result.user;
-  $("#login-screen").fadeOut(function(){
-  $("#chat-screen").fadeIn();
-});
 
-  messegesRef.on("value", function(snapshot) {
-    console.log(snapshot.val());
-    $("#messages").html("");
-    var values = snapshot.val();
-    for(var msgId in values) {
-      var msg = values[msgId];
-      $("#messages").append(`
-        <li><strong>${msg.user}</strong>: ${msg.messege}</li>
-        `);
-      }
+function sendMessege(username) {
+    messegesRef.push({
+        user: username,
+        messege: $("#messege-input").val()
     });
-    document.addEventListening("keydown", function(e){
-      if(e.keyCode === 13) sendMessege(user.displayName);
-    });
+}
+$(".btn-facebook").click(function() {
+  var provider = new firebase.auth.FacebookAuthProvider();
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+      var token = result.credential.accessToken;
+      var user = result.user;
+      $(".login-screen").fadeOut(function() {
+          $("#chat-screen").fadeIn();
+      });
+
+      messegesRef.on("value", function(snapshot) {
+          console.log(snapshot.val());
+          $("#messages").html("");
+          var values = snapshot.val();
+          for (var msgId in values) {
+              var msg = values[msgId];
+              $("#messages").append(`
+          <li><strong>${msg.user}</strong>: ${msg.messege}</li>
+          `);
+          }
+      });
+      document.addEventListener("keydown", function(e) {
+          if (e.keyCode === 13){
+            sendMessege(user.displayName);
+            $("#messege-input").val("");
+          }
+      });
 
   }).catch(function(error) {
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  console.log(errorCode + " - " + errorMessage);
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorCode + " - " + errorMessage);
+  });
 });
-function sendMessege() {
-messegesRef.push({
-  user: username,
-  messege: $("#messege-input").val()
-});
-}
